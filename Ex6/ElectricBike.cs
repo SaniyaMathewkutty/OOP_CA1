36934dccb78d09032ed47bcf1d3ca4c3ae696d24
Saniya Mathewkutty <S00223097@atu.ie>
2022-10-20 11:22:41 +0100

    Created and edited class for ex6

---
+++ Ex6/ElectricBike.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class ElectricBike
+    {
+        // Attributes
+        private string _id;
+        private string _location;
+        private decimal _battery;
+
+        // Parameterised constructor
+        public ElectricBike(string id, string location, decimal battery)
+        {
+            this._id = id;
+            this._location = location;
+            this._battery = battery;
+        }
+
+        // Properties
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        public decimal BatteryPercentage
+        {
+            get { return _batterypercentage; }
+            set { _batterypercentage = value; }
+        }
+    }
+}

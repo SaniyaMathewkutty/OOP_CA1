6e141f1ae959fc6a48f424d247ca3643185a90ae
Saniya Mathewkutty <S00223097@atu.ie>
2022-10-20 11:17:34 +0100

    Created and edited ex5

---
+++ Ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = 50;
+            GradeIncrease(ref grade);
+            Console.Write(grade);
+
+        }
+        static void GradeIncrease(ref int newGrade)
+        {
+            newGrade += newGrade / 15;
+        }
+    }
+}

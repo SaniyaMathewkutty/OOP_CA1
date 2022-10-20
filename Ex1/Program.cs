57a4dc22f51248c36a19b36d7640279387a2d4ab
Saniya Mathewkutty <S00223097@atu.ie>
2022-10-20 10:19:48 +0100

    Ex1 completed, switch statement in ex2 included

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,7 +10,17 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            // Get input from user
+            Console.WriteLine("Enter temperature : ");
+            int temperature = int.Parse(Console.ReadLine());
 
+            // Validate number and display message
+            if (temperature >= 18 && temperature <= 22)
+                Console.WriteLine("Temperature is in the normal range.");
+            else
+                Console.WriteLine("The temperature is outside the normal range");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Ex2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            string memberType;
+            double discount = 0, charge = 50, price = 0;
+
+            // Get input from user and initialise variables
+            Console.Write("Enter the type of member you are : ");
+            memberType = Console.ReadLine().ToLower();
+
+            // Switch statement to determine discount
+            switch (memberType)
+            {
+                case "children":
+                    discount = 0.5;
+                    break;
+                case "students":
+                    discount = 0.5;
+                    break;
+                case "oap":
+                    discount = 0.5;
+                    break;
+                default:
+                    discount = 0;
+                    break;
+            }
+
+            // Calculate price including discount (if any)
+            price = charge - (charge * discount);
+
+            // Display discounted price
+            Console.Write($"Your price = € {price}");
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
